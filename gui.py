5f1a1a8bfd9110aca9fca41cdab70e3d306787c4
anterakotoarison <113263127+anterakotoarison@users.noreply.github.com>
2023-10-07 11:38:30 -0600

    headers

--- gui.py
+++ gui.py
@@ -2,3 +2,9 @@
 # Canadian Space Agency Challenge 1 (Moonwalker)
 # Written by Nicole Tan and Antenaina Rakotoarison
 # October 7 - 8 2023
+#gui.py
+
+#This file contains the gui code
+
+
+#import libraries
