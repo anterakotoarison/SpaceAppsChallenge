e03d67e29c0f4233cc4a8ef6e9556206f3088d59
anterakotoarison <113263127+anterakotoarison@users.noreply.github.com>
2023-10-07 20:07:05 -0600

    tried bgd images
    not good :(

--- gui.py
+++ gui.py
@@ -1,12 +1,10 @@
 import tkinter as tk
-import ttkbootstrap as ttk
 from PIL import Image, ImageTk
 from tkinter import PhotoImage
 
 # Create the main application window
 root = tk.Tk()
-style = ttk.Style()
-style.theme_use("clam")
+
 # Set the window title
 root.title("Space Date Explore Application")
 
@@ -65,12 +63,23 @@ def open_explore_page():
 def open_new_window():
     for widget in root.winfo_children():
         widget.destroy()
+
+    background_image = PhotoImage(file="nextbgd.png")  # Replace with the path to your image
+
+    # Create a label to display the background image
+    background_label = tk.Label(root, image=background_image)
+    background_label.place(relwidth=1, relheight=1)  
+
+    # Create other widgets (labels, buttons, etc.) here
+
+    # Keep a reference to the background image
+    root.background_image = background_image
     
     initial_width = 1070
     initial_height = 1050
     root.geometry(f"{initial_width}x{initial_height}")
     # Create content for the new window
-    location_label = tk.Label(root, text="Insert your location")
+    location_label = tk.Label(root, text="Insert your location", padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     location_label.pack()
     location_label.place(relx=0.5, rely=0.27, anchor="center")
 
@@ -78,7 +87,7 @@ def open_new_window():
     location_entry.pack()
     location_entry.place(relx=0.5, rely=0.3, anchor="center")
     
-    date_label = tk.Label(root, text="Insert your data")
+    date_label = tk.Label(root, text="Insert your data", padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     date_label.pack()
     date_label.place(relx=0.5, rely=0.53, anchor="center")
 
@@ -86,28 +95,39 @@ def open_new_window():
     date_entry.pack()
     date_entry.place(relx=0.5, rely=0.56, anchor="center")
 
-    next_button = tk.Button(root, text="NEXT", command=open_explore_page)
+    next_button = tk.Button(root, text="NEXT", command=open_explore_page, padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     next_button.pack()
     next_button.place(relx=0.5, rely=0.65, anchor="center")
 
-    back_button = tk.Button(root, text="BACK", command=open_main_window)
+    back_button = tk.Button(root, text="BACK", command=open_main_window, padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     back_button.pack()
     back_button.place(relx=0.05, rely=0.05, anchor="center")
 
 def open_main_window():
     for widget in root.winfo_children():
         widget.destroy()
+
+    background_image = PhotoImage(file="mainbackground.png")  # Replace with the path to your image
+
+    # Create a label to display the background image
+    background_label = tk.Label(root, image=background_image)
+    background_label.place(relwidth=1, relheight=1)  
+
+    # Create other widgets (labels, buttons, etc.) here
+
+    # Keep a reference to the background image
+    root.background_image = background_image
     # Create a Label for "WELCOME"
-    welcome_label = tk.Label(root, text="WELCOME", font=("Helvetica", 24))
+    welcome_label = tk.Label(root, text="WELCOME", padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     # Place it in the center of the window
     welcome_label.place(relx=0.5, rely=0.4, anchor="center")
 
     # Create a Button for "START" that opens a new window
-    start_button = tk.Button(root, text="START", command=open_new_window)
+    start_button = tk.Button(root, text="START", command=open_new_window, padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     # Place it below the "WELCOME" label
     start_button.place(relx=0.5, rely=0.45, anchor="center")
 
-    exit_button = tk.Button(root, text="EXIT", command=exit_app)
+    exit_button = tk.Button(root, text="EXIT", command=exit_app, padx=0, pady=0, borderwidth=0, highlightthickness=0, compound="top")
     exit_button.place(relx=0.05, rely=0.05, anchor="center")
 
 def main():
